87e66893d6c0b5f5d249cff9780859fe80279ab0
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-05 16:59:11 +0100

    snack1

---
+++ jsnack1/script.js
@@ -0,0 +1,30 @@
+// Snack 1
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50
+
+// Array
+const numberList = [];
+
+let somma = 0;
+
+// do {
+
+//     const userNumber = Number(prompt("Inserisci un numero"));
+
+//     if(somma < 50) {
+
+//         numberList.push(userNumber);
+
+//         somma += userNumber;
+//     }
+
+// } while (somma < 50);
+
+
+while (somma < 50) {
+    
+    numberList.push(userNumber);
+
+    somma += userNumber;
+}
+
+console.log (somma)
