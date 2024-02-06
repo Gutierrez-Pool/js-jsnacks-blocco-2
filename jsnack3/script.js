0446c7c33bbd5c6e489faa6b2673e3e52321a71c
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-06 16:23:52 +0100

    snack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,7 +2,30 @@
 
 // Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro. (Se iniziate con un array da 3 elementi e l'altro da 5, eseguite istruzioni sinchè non ne hanno entrambi 5
 
-const gruppoUno = [uno, due, tre];
 
-const gruppoDue = [uno, dos, tres, cuatro, cinco];
+// Primo array
+const gruppo2 = [3,6,9];
+// Secondo array
+const gruppo1 = [1,2,3,4,5,6,7,5,7];
 
+
+if(gruppo1.length < gruppo2.length) {
+
+    while (gruppo1.lenght < gruppo2.length) {
+        
+        gruppo1.push(gruppo2[gruppo1.length]);
+    
+    }
+
+} else {
+
+    while (gruppo2.length < gruppo1.length) {
+        
+        gruppo2.push(gruppo1[gruppo2.length]);
+    
+    }
+
+}
+
+console.log (gruppo1);
+console.log (gruppo2);
