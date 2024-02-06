4a17440e6c944935237d2609e1ffca07657c3add
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-06 16:41:40 +0100

    snack4

--- jsnack4/script.js
+++ jsnack4/script.js
@@ -1,3 +1,18 @@
 // Snack 4
 
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let userNumber = Number(prompt("Scegli un numero"));
+
+console.log ("User number:", userNumber);
+
+
+if (userNumber % 2 == 0) {
+
+    console.log("Numero pari:", userNumber);
+
+} else {
+
+    console.log("Numero successivo:", userNumber + 1);
+
+}
